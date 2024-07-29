3820776c2a429f0bd79c9e8f9a8d3826bbf32fef
manojworkhub <db9238422@gmail.com>
2024-07-29 17:16:16 +0530

    format chaged

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,33 @@
 import { BrowserRouter } from "react-router-dom";
-import { About, Contact, Experience, Hero, Navbar, Tech, Works, StarsCanvas } from "./components";
+import {
+  About,
+  Contact,
+  Experience,
+  Hero,
+  Navbar,
+  Tech,
+  Works,
+  StarsCanvas,
+} from "./components";
 
 function App() {
   return (
     <BrowserRouter>
       <div className="relative z-0 bg-[#050816]">
-     
         <div className="w-[100%] h-[100%] bg-hero-pattern md:h-[70cap]   landscapeL:h-[100vh] min-[720px]:h-[600px]   z-20 bg-cover bg-no-repeat bg-center">
           <Navbar />
           <Hero />
-          
         </div>
-        <About/>
-        <Experience/>
-        <Tech/> 
-        <StarsCanvas/>
-        <Works/>
+        <About />
+        <Experience />
+        <Tech />
+        <StarsCanvas />
+        <Works />
         {/* <Feedbacks/> */}
         <div className="relative z-0">
-          <Contact/>
-          <StarsCanvas/>
+          <Contact />
+          <StarsCanvas />
         </div>
-      
       </div>
     </BrowserRouter>
   );
